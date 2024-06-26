9bc7df6823d3bfe33bb2d2238a969c32dd0a9928
marcosdima <marcosdimatteo.dev@gmail.com>
2024-06-26 12:59:51 -0300

    mod: detail shortcut version

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404, HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -23,11 +23,16 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 
+'''
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+'''
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
