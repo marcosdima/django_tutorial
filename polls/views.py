198347bf9ff17ecd26fb1c351673d16f17c8ad0d
marcosdima <marcosdimatteo.dev@gmail.com>
2024-06-26 12:20:25 -0300

    mod: using render shorcut

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,27 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
+'''
+With HttpResponse and loader:
+    def index(request):
+        latest_question_list = Question.objects.order_by("-pub_date")[:5]
+        template = loader.get_template("polls/index.html")
+        context = {
+            "latest_question_list": latest_question_list,
+        }
+        return HttpResponse(template.render(context, request))
+
+With render HttpResponse and loader no longer will be needed.
+'''
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
